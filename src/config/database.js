53d2ea79ec42a91ca00815cc3c1b47f473f0a682
Samuel Juren <sammjureen@outlook.com>
2025-07-29 14:11:06 -0300

    configuração database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "123",
+  database: "dbtestedocker",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
